e03a409548720aac22527d064d48df81ce2d4d30
patriciaong977 <patricia.ong977@gmail.com>
2023-01-25 13:45:53 -0600

    Home Component

---
+++ src/components/home/HeaderSocials.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className="home__socials">
+
+      {/* LinkedIn */}
+      <a href="https://www.linkedin.com/in/patriciaong977" className="home__social-link" target="_blank">
+        <i class="fa-brands fa-linkedin"></i>
+      </a>
+
+      {/* Github */}
+      <a href="https://www.github.com/patriciaong977" className="home__social-link" target="_blank">
+        <i class="fa-brands fa-github"></i>
+      </a>
+
+    </div>
+  )
+}
+
+export default HeaderSocials
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,15 +2,27 @@ import React from 'react';
 import "./home.css";
 
 import Me from "../../assets/isha.jpg";
+import HeaderSocials from './HeaderSocials';
+import ScrollDown from './ScrollDown';
+
 
 const Home = () => {
   return (
     <section className="home container" id="home">
       <div className="intro">
+
         <img src={Me} alt="" className="home___img" style={{borderRadius: '50%'}}/>
         <h1 className="home__name">Patricia Ong</h1>
-        <span className="home__education">Recent Graduate</span>
+        <span className="home__education">Welcome!</span>
+
+        <HeaderSocials />
+
+        <a href="#contact" className="btn">Hire Me!</a>
+
+        <ScrollDown />
+
       </div>
+
     </section>
   )
 }
