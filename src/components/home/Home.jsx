1fd2035f40946f3510338a4a27f501e441f3680b
patriciaong977 <patricia.ong977@gmail.com>
2023-02-03 15:14:26 -0600

    Replaced home name with TypeAnimation

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,6 +4,7 @@ import "./home.css";
 import Me from "../../assets/isha.jpg";
 import HeaderSocials from './HeaderSocials';
 import ScrollDown from './ScrollDown';
+import { TypeAnimation } from 'react-type-animation';
 
 
 const Home = () => {
@@ -11,9 +12,28 @@ const Home = () => {
     <section className="home container" id="home">
       <div className="intro">
 
-        <img src={Me} alt="" className="home___img" style={{borderRadius: '50%'}}/>
-        <h1 className="home__name">Patricia Ong</h1>
-        <span className="home__education">Welcome!</span>
+        <img src={Me} alt="" className="home___img" style={{ borderRadius: '50%' }} />
+
+        {/* <h1 className="home__name">Patricia Ong</h1>
+        <span className="home__education">Welcome!</span> */}
+
+        <TypeAnimation
+              sequence={[
+                'Hello! 👋',
+                1000, // Waits 1s
+                '🙋‍♀️ I am',
+                1000, // Waits 2s
+                'Patricia Ong',
+                4000,
+                () => {
+                  console.log('Done typing!');
+                }
+              ]}
+              wrapper="div"
+              cursor={true}
+              repeat={Infinity}
+              style={{ fontSize: 'var(--h1-font-size)', fontWeight: 'var(--font-bold)', marginTop: '1.5rem', marginBottom: '0.5rem', color: 'var(--title-color)'}}
+            />
 
         <HeaderSocials />
 
