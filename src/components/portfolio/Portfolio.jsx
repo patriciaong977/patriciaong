9a7e5546654747ad7fcec845373a6f36bfe4ded3
patriciaong977 <patricia.ong977@gmail.com>
2023-02-15 16:17:42 -0600

    Fixed sidebar link to portfolio.

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -12,7 +12,7 @@ const Portfolio = () => {
     setItems(updatedItems);
   }
   return (
-    <section className="work container section">
+    <section className="work container section" id='portfolio'>
       <h2 className="section__title">Recent Works</h2>
 
       <div className="work__filters">
