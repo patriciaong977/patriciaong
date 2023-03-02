de3ffae1292ec3559387d01c5c99fdc217a7db66
patriciaong977 <patricia.ong977@gmail.com>
2023-03-02 16:58:13 -0600

    Changing Resume Section

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -1,50 +1,63 @@
-import React from 'react';
-import "./resume.css";
+import React from 'react'
+import "./resume.css"
 
-import Data from "./Data";
-import Card from "./Card";
 
 const Resume = () => {
   return (
-    <section className="resume container section" id="resume">
-      <h2 className="section__title">Experience</h2>
-      <div className="resume__container grid">
-        <div className="timeline grid">
-          {Data.map((val, id) => {
-            if (val.category === "education") {
-              return (
-                <Card
-                  key={id}
-                  icon={val.icon}
-                  title={val.title}
-                  school={val.school}
-                  year={val.year}
-                  desc={val.desc}
-                  awards={val.awards}
-                  organizations={val.organizations}
-                />
-              )
-            }
-          })}
+    <section className="resume section">
+      <h2 className="section__title">Journey</h2>
+      {/* <span className="section__subtitle">How it's been so far.</span> */}
+
+      <div className="resume__container container">
+        <div className="resume__tabs">
+          <div className="resume__button resume__active button--flex">
+            <i className="icon-graduation resume__icon"></i> {" "}Education
+          </div>
+
+          <div className="resume__button button--flex">
+            <i className="icon-briefcase resume__icon"></i> {" "}Experience
+          </div>
         </div>
 
-        <div className="timeline grid">
-          {Data.map((val, index) => {
-            if (val.category === "experience") {
-              return (
-                <Card
-                  key={index}
-                  icon={val.icon}
-                  title={val.title}
-                  company={val.company}
-                  year={val.year}
-                  desc={val.desc}
-                />
-              )
-            }
-          })}
-        </div>
+        <div className="resume__sections">
+          <div className="resume__content">
+            {/* Education */}
+            <div className="resume__data">
+              <div>
+                <h3 className="resume__title"></h3>
+                <span className="resume__subtitle">California State University, Fullerton</span>
+                  <div className="resume__date">
+                    <i className="icon-calendar "></i> 2022 - Present
+                  </div>
+              </div>
 
+              <div>
+                <span className="resume__rounder"></span>
+                <span className="resume__line"></span>
+              </div>
+            </div>
+
+            {/* Experience */}
+            <div className="resume__data">
+              <div></div>
+
+              <div>
+                <span className="resume__rounder"></span>
+                <span className="resume__line"></span>
+              </div>
+
+              <div>
+                <h3 className="resume__title"></h3>
+                <span className="resume__subtitle">California State University, Fullerton</span>
+                  <div className="resume__date">
+                    <i className="icon-calendar "></i> 2022 - Present
+                  </div>
+              </div>
+
+
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   )
